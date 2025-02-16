a4cc3e23ac98f33de4559aa0263de08bbb96f5e2
prasannadeviramesh <138461679+prasannadeviramesh@users.noreply.github.com>
2025-02-16 23:05:52 +0530

    Add files via upload

---
+++ Programs/DateandTime.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class DateandTime {
+    public static void main(String []args){
+        Date today=new Date();
+        System.out.println("Today date and time = "+today);
+
+    }    
+}

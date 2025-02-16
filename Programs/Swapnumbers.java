bec02ec519a3d9d759d547781de24f0ca3b4635f
prasannadeviramesh <138461679+prasannadeviramesh@users.noreply.github.com>
2025-02-16 23:03:39 +0530

    Add files via upload

---
+++ Programs/Swapnumbers.java
@@ -0,0 +1,11 @@
+public class Swapnumbers {
+    public static void main(String[]args){
+        int a=10;
+        int b=20;        
+        System.out.println("Before swapping\na="+a+"\nb="+b);
+        int temp=a;
+        a=b;
+        b=temp;
+        System.out.println("Before swapping\na="+a+"\nb="+b);
+    }
+}

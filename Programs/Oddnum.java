3ac16f48611f0e205145be0629fb8fff5d58b5e1
prasannadeviramesh <138461679+prasannadeviramesh@users.noreply.github.com>
2025-02-16 23:02:29 +0530

    Add files via upload

---
+++ Programs/Oddnum.java
@@ -0,0 +1,11 @@
+public class Oddnum {
+    public static void main(String []args){
+        int sum = 11;
+        if(sum%2!=0){
+            System.out.println("Number is a Odd");
+        }
+        else{
+            System.out.println( "Number is not Odd");
+        }
+    }
+}

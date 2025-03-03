84e88a1be73663b52a834d90b3f4d19316bde0b8
prasannadeviramesh <138461679+prasannadeviramesh@users.noreply.github.com>
2025-03-03 09:15:36 +0530

    Add files via upload

---
+++ Programs/Continue.java
@@ -0,0 +1,13 @@
+public class Continue {
+    public static void main(String []args){
+        int i=0;
+        while(i<15){
+            i++;
+            if (i%2==0){
+                continue;
+               
+            }
+            System.out.println(i);
+        }
+    }
+}
